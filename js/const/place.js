07aef0824f2e3f5de7d5795b2025ee889d4cc5a9
SvetlanaDanilchyk <danilchyk.svet@gmail.com>
2025-12-20 21:21:31 +0100

    Add categories section rendering via JavaScript. Added the ability to open and close the menu

---
+++ js/const/place.js
@@ -0,0 +1,91 @@
+export const category = [
+    {
+        id:0,
+        title: "Solo Travel",
+        img: "../img/categories/solo.jpg"
+    },
+     {
+        id:1,
+        title: "Mount Travel",
+        img: "../img/categories/mount.jpg"
+    },
+     {
+        id:2,
+        title: "Jungal Travel",
+        img: "../img/categories/jungal.jpg"
+    },
+     {
+        id:3,
+        title: "Road Travel",
+        img: "../img/categories/road.jpg"
+    },
+     {
+        id:4,
+        title: "Osean Travel",
+        img: "../img/categories/osean.jpg"
+    },
+     {
+        id:6,
+        title: "Old City Travel",
+        img: "../img/categories/old-city.jpg"
+    },
+     {
+        id:7,
+        title: "Sea Travel",
+        img: "../img/categories/sea.jpg"
+    },
+
+]
+
+export const featuredExplore = [
+    {
+        img: "./img/featured-posts/img-1.jpg",
+        title: "The Golden Sands of Florida and clifornia",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit. In sed etdonec purus viverra. Sit justo velit, eu sed",
+        subtitle: "Solo Travel",
+        autorName: "Adam Smith"
+    },
+      {
+        img: "./img/featured-posts/img-2.jpg",
+        title: "The Golden Sands of Florida and clifornia",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit. In sed etdonec purus viverra. Sit justo velit, eu sed",
+        subtitle: "Solo Travel",
+        autorName: "Vera Smith"
+    },
+      {
+        img: "./img/featured-posts/img-3.jpg",
+        title: "The Golden Sands of Florida and clifornia",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit. In sed etdonec purus viverra. Sit justo velit, eu sed",
+        subtitle: "Solo Travel",
+        autorName: "Tom Readl"
+    },
+      {
+        img: "./img/featured-posts/img-4.jpg",
+        title: "The Golden Sands of Florida and clifornia",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit. In sed etdonec purus viverra. Sit justo velit, eu sed",
+        subtitle: "Solo Travel",
+        autorName: "Riana Den"
+    },
+      {
+        img: "./img/featured-posts/img-5.jpg",
+        title: "The Golden Sands of Florida and clifornia",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit. In sed etdonec purus viverra. Sit justo velit, eu sed",
+        subtitle: "Solo Travel",
+        autorName: "Mik Warem"
+    },
+      {
+        img: "./img/featured-posts/img-6.jpg",
+        title: "The Golden Sands of Florida and clifornia",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit. In sed etdonec purus viverra. Sit justo velit, eu sed",
+        subtitle: "Solo Travel",
+        autorName: "Kira Soth"
+    },
+      {
+        img: "./img/featured-posts/img-2.jpg",
+        title: "Mount Travel",
+        text: "Lorem ipsum dolor sit amet, con turadip iscingelit.",
+        subtitle: "Mount Travel",
+        autorName: "Svet Dan"
+    },
+]
+
--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-new Swiper(".swiper", {
-  slidesPerView: 3,
-  grid: {
-    rows: 3,
-  },
-  mousewheel: {
-    forceToAxis: true,
-  },
+import {category, featuredExplore } from './const/place.js';
+
+const menuHamburger = document.querySelector('.menu__hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const close = document.querySelector('.mobile-menu__btn-close');
+const sliderItems = document.getElementById('category');
+
+
+menuHamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'grid';
 });
+
+close.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+});
+
+console.log(sliderItems);
+sliderItems.innerHTML = category.map(item => `
+    <div class="slider__item" data-id="${item.id}">
+      <img src="${item.img}" alt="${item.title}">
+      <h3>${item.title}</h3>
+    </div>
+  `)
+  .join("");
+
+/* for (let i = 0; i < 4; i++) {
+  sliderItems.innerHTML += `<div id=${data[i].id} class="slider__item col-lg-4 col-md-4 col-xs-3 js-slider"></div>`;
+}
+
+const itemsSlider = document.querySelectorAll(".js-slider");
+
+itemsSlider.forEach((item, index) => {
+  item.innerHTML += `<img class="slider__img col-lg-12" src=${data[index].imageUrl}>`;
+  item.innerHTML +=
+    `<div class="slider__descr">` +
+    `<div class="slider__descr-header">${data[index].name}</div>` +
+    `<div class="slider__descr-text">${data[index].city},${data[index].country}</div></div>`;
+}); */
